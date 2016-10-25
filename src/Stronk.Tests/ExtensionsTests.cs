d42b36bcab9227589460f28ea1fcbf20d0519b0a
andy <andy@andydote.co.uk>
2016-10-25 20:28:15 +0300

    extensions: simplest implementation of fromAppConfig()

---
+++ src/Stronk.Tests/ExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using Shouldly;
+using Xunit;
+
+namespace Stronk.Tests
+{
+	public class ExtensionsTests
+	{
+		[Fact]
+		public void When_loading_the_configuration()
+		{
+			var config = new Config();
+			config.FromAppConfig();
+
+			config.ShouldSatisfyAllConditions(
+				() => config.Name.ShouldBe(ConfigurationManager.AppSettings["Name"]),
+				() => config.Version.ShouldBe(Convert.ToInt32(ConfigurationManager.AppSettings["Version"])),
+				() => config.Environment.ShouldBe((TargetEnvironment)Enum.Parse(typeof(TargetEnvironment), ConfigurationManager.AppSettings["Environment"], true))
+			);
+		}
+
+		public class Config
+		{
+			public string Name { get; private set; }
+			public int Version { get; private set; }
+			public TargetEnvironment Environment { get; private set; }
+		}
+
+		public enum TargetEnvironment
+		{
+			Dev,
+			Test,
+			ExternalTest,
+			QA,
+			Production
+		}
+	}
+}
---
+++ src/Stronk/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+
+namespace Stronk
+{
+	public static class Extensions
+	{
+		public static void FromAppConfig(this object target)
+		{
+			var properties = target
+				.GetType()
+				.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+			var appSettings = ConfigurationManager.AppSettings;
+
+			foreach (var property in properties)
+			{
+				var hasSetting = appSettings.AllKeys.Contains(property.Name);
+
+				if (hasSetting)
+				{
+					var value = appSettings[property.Name];
+
+					var converted = property.PropertyType.IsEnum
+						? Enum.Parse(property.PropertyType, value, ignoreCase: true)
+						: Convert.ChangeType(value, property.PropertyType);
+
+					property.GetSetMethod(true).Invoke(target, new[] { converted });
+				}
+			}
+		}
+	}
+}
