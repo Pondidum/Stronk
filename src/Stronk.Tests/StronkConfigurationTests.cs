d335083f3cd6cf75ed65abbc3377c3277ea1fcbb
andy <andy@andydote.co.uk>
2016-10-29 21:14:00 +0300

    configuration: check collections are initialised

---
+++ src/Stronk.Tests/StronkConfigurationTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Shouldly;
+using Stronk.PropertySelection;
+using Stronk.ValueConversion;
+using Stronk.ValueSelection;
+using Xunit;
+
+namespace Stronk.Tests
+{
+	public class StronkConfigurationTests
+	{
+		[Fact]
+		public void By_default_the_collections_are_initialised()
+		{
+			var config = new StronkConfiguration();
+			var converters = config.Converters.Select(c => c.GetType());
+			var propertySelectors = config.PropertySelectors.Select(c => c.GetType());
+			var valueSelectors = config.ValueSelectors.Select(t => t.GetType());
+
+			config.ShouldSatisfyAllConditions(
+				() => converters.ShouldBe(new[] { typeof(LambdaValueConverter<Uri>), typeof(EnumValueConverter), typeof(FallbackValueConverter) }),
+				() => propertySelectors.ShouldBe(new[] { typeof(PrivateSetterPropertySelector), typeof(BackingFieldPropertySelector) }),
+				() => valueSelectors.ShouldBe(new[] { typeof(PropertyNameValueSelector) })
+			);
+		}
+	}
+}
