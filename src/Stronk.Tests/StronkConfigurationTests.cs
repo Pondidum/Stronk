e46f58c7af37e0d81b1effbdc75e1520c1de89c4
andy <andy@andydote.co.uk>
2016-10-31 21:10:41 +0200

    stronkConfiguration: refactor tests

--- src/Stronk.Tests/StronkConfigurationTests.cs
+++ src/Stronk.Tests/StronkConfigurationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Shouldly;
 using Stronk.PropertySelection;
@@ -10,15 +11,21 @@ namespace Stronk.Tests
 {
 	public class StronkConfigurationTests
 	{
+		private readonly StronkConfiguration _config;
+
+		public StronkConfigurationTests()
+		{
+			_config = new StronkConfiguration();
+		}
+
 		[Fact]
 		public void By_default_the_collections_are_initialised()
 		{
-			var config = new StronkConfiguration();
-			var converters = config.ValueConverters.Select(c => c.GetType());
-			var propertySelectors = config.PropertySelectors.Select(c => c.GetType());
-			var valueSelectors = config.ValueSelectors.Select(t => t.GetType());
+			var converters = _config.ValueConverters.Select(c => c.GetType());
+			var propertySelectors = _config.PropertySelectors.Select(c => c.GetType());
+			var valueSelectors = _config.ValueSelectors.Select(t => t.GetType());
 
-			config.ShouldSatisfyAllConditions(
+			_config.ShouldSatisfyAllConditions(
 				() => converters.ShouldBe(new[] { typeof(LambdaValueConverter<Uri>), typeof(EnumValueConverter), typeof(FallbackValueConverter) }),
 				() => propertySelectors.ShouldBe(new[] { typeof(PrivateSetterPropertySelector), typeof(BackingFieldPropertySelector) }),
 				() => valueSelectors.ShouldBe(new[] { typeof(PropertyNameValueSelector) })
@@ -28,50 +35,57 @@ namespace Stronk.Tests
 		[Fact]
 		public void When_a_value_converter_is_added()
 		{
-			var config = new StronkConfiguration();
-			config.Add(new DtoValueConverter());
+			_config.Add(new DtoValueConverter());
 
-			config.ValueConverters.Last().ShouldBeOfType<DtoValueConverter>();
+			_config.ValueConverters.Last().ShouldBeOfType<DtoValueConverter>();
 		}
 
 		[Fact]
 		public void When_a_value_converter_is_added_before_an_existing_converter()
 		{
-			var config = new StronkConfiguration();
-			config.AddBefore<LambdaValueConverter<Uri>>(new DtoValueConverter());
+			_config.AddBefore<LambdaValueConverter<Uri>>(new DtoValueConverter());
 
-			var converters = config.ValueConverters.Select(c => c.GetType()).ToList();
-			var searchIndex = converters.IndexOf(typeof(LambdaValueConverter<Uri>));
-			var insertIndex = converters.IndexOf(typeof(DtoValueConverter));
-
-			insertIndex.ShouldBe(searchIndex - 1);
+			InsertIndexShouldBeBefore(_config.ValueConverters, typeof(LambdaValueConverter<Uri>), typeof(DtoValueConverter));
 		}
 
 		[Fact]
 		public void When_a_value_converter_is_added_after_an_existing_converter()
 		{
-			var config = new StronkConfiguration();
-			config.AddAfter<LambdaValueConverter<Uri>>(new DtoValueConverter());
+			_config.AddAfter<LambdaValueConverter<Uri>>(new DtoValueConverter());
 
-			var converters = config.ValueConverters.Select(c => c.GetType()).ToList();
-			var searchIndex = converters.IndexOf(typeof(LambdaValueConverter<Uri>));
-			var insertIndex = converters.IndexOf(typeof(DtoValueConverter));
-
-			insertIndex.ShouldBe(searchIndex + 1);
+			InsertIndexShouldBeAfter(_config.ValueConverters, typeof(LambdaValueConverter<Uri>), typeof(DtoValueConverter));
 		}
 
 		[Fact]
 		public void When_a_value_converter_is_added_before_a_non_existing_converter()
 		{
-			var config = new StronkConfiguration();
-			Should.Throw<StronkConfigurationException>(() => config.AddBefore<LambdaValueConverter<object>>(new DtoValueConverter()));
+			Should.Throw<StronkConfigurationException>(() => _config.AddBefore<LambdaValueConverter<object>>(new DtoValueConverter()));
 		}
 
 		[Fact]
 		public void When_a_value_converter_is_added_after_a_non_existing_converter()
 		{
-			var config = new StronkConfiguration();
-			Should.Throw<StronkConfigurationException>(() => config.AddAfter<LambdaValueConverter<object>>(new DtoValueConverter()));
+			Should.Throw<StronkConfigurationException>(() => _config.AddAfter<LambdaValueConverter<object>>(new DtoValueConverter()));
+		}
+
+
+
+		private static void InsertIndexShouldBeBefore<T>(IEnumerable<T> collection, Type search, Type inserted)
+		{
+			var converters = collection.Select(c => c.GetType()).ToList();
+			var searchIndex = converters.IndexOf(search);
+			var insertIndex = converters.IndexOf(inserted);
+
+			insertIndex.ShouldBe(searchIndex - 1);
+		}
+
+		private static void InsertIndexShouldBeAfter<T>(IEnumerable<T> collection, Type search, Type inserted)
+		{
+			var converters = collection.Select(c => c.GetType()).ToList();
+			var searchIndex = converters.IndexOf(search);
+			var insertIndex = converters.IndexOf(inserted);
+
+			insertIndex.ShouldBe(searchIndex + 1);
 		}
 
 		private class Dto { }
