64e07feb0ac06e76303801922e06bbdf45b8d32b
andy <andy@andydote.co.uk>
2016-11-01 23:31:57 +0200

    tests: add simple testapp for easier perf testing

---
+++ src/TestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stronk;
+
+namespace TestApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var config = new MassiveConfig();
+			var configuration = new StronkConfiguration();
+			var provider = new AppConfigProvider();
+
+			config.FromAppConfig(configuration, provider);
+		}
+	}
+}
