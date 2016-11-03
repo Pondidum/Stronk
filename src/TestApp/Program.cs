a4a6d1e77076f89f2287a5bf9bd8f6028b521bfd
andy <andy@andydote.co.uk>
2016-11-03 23:14:24 +0200

    testApp: refactor to remove file io from performance testing

--- src/TestApp/Program.cs
+++ src/TestApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +15,24 @@ namespace TestApp
 		{
 			var config = new MassiveConfig();
 			var configuration = new StronkConfiguration();
-			var provider = new AppConfigProvider();
+			var provider = new InMemoryProvider();
 
 			config.FromAppConfig(configuration, provider);
+		}
+	}
+
+	public class InMemoryProvider : IConfigurationProvider
+	{
+		public NameValueCollection AppSettings { get; }
+		public ConnectionStringSettingsCollection ConnectionStrings { get; }
+
+		public InMemoryProvider()
+		{
+			AppSettings = new NameValueCollection();
+			ConnectionStrings = new ConnectionStringSettingsCollection();
+
+			for (int i = 0; i < 200; i++)
+				AppSettings["SomeProperty" + i] = i.ToString();
 		}
 	}
 }
