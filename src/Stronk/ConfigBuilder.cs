a5d3bcfd5fc6d23c88a9ab0b37e2c58311575301
andy <andy@andydote.co.uk>
2016-11-18 19:56:34 +0200

    extensions: extract logic into its own class

---
+++ src/Stronk/ConfigBuilder.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using Stronk.PropertySelection;
+using Stronk.ValueConversion;
+using Stronk.ValueSelection;
+
+namespace Stronk
+{
+	public class ConfigBuilder
+	{
+		private readonly IStronkConfiguration _configuration;
+
+		public ConfigBuilder(IStronkConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public void Populate(object target, IConfigurationProvider configProvider = null)
+		{
+			var valueSelectors = _configuration.ValueSelectors.ToArray();
+			var converters = _configuration.ValueConverters.ToArray();
+
+			var properties = _configuration
+				.PropertySelectors
+				.SelectMany(selector => selector.Select(target.GetType()));
+
+			var args = new ValueSelectorArgs(configProvider ?? new AppConfigProvider());
+
+			foreach (var property in properties)
+			{
+				var value = GetValueFromSource(valueSelectors, args, property);
+
+				if (value == null)
+					continue;
+
+				IValueConverter converter = null;
+
+				foreach (var valueConverter in converters)
+				{
+					if (valueConverter.CanMap(property.Type) == false)
+						continue;
+
+					converter = valueConverter;
+					break;
+				}
+
+				if (converter == null)
+					continue;
+
+				var converted = converter.Map(property.Type, value);
+
+				property.Assign(target, converted);
+			}
+		}
+
+		private static string GetValueFromSource(IValueSelector[] valueSelectors, ValueSelectorArgs args, PropertyDescriptor property)
+		{
+			foreach (var filter in valueSelectors)
+			{
+				var value = filter.Select(args.With(property));
+
+				if (value != null)
+					return value;
+			}
+
+			return null;
+		}
+	}
+}
--- src/Stronk/Extensions.cs
+++ src/Stronk/Extensions.cs
@@ -1,10 +1,4 @@
-﻿using System.Linq;
-using System.Runtime.CompilerServices;
-using Stronk.PropertySelection;
-using Stronk.ValueConversion;
-using Stronk.ValueSelection;
-
-namespace Stronk
+﻿namespace Stronk
 {
 	public static class Extensions
 	{
@@ -15,53 +9,9 @@ namespace Stronk
 
 		public static void FromAppConfig(this object target, IStronkConfiguration configuration, IConfigurationProvider configProvider = null)
 		{
-			var propertySelectors = configuration.PropertySelectors;
-			var valueSelectors = configuration.ValueSelectors.ToArray();
-			var converters = configuration.ValueConverters.ToArray();
+			var builder = new ConfigBuilder(configuration);
 
-			var properties = propertySelectors
-				.SelectMany(selector => selector.Select(target.GetType()));
-
-			var args = new ValueSelectorArgs(configProvider ?? new AppConfigProvider());
-
-			foreach (var property in properties)
-			{
-				var value = GetValueFromSource(valueSelectors, args, property);
-
-				if (value == null)
-					continue;
-
-				IValueConverter converter = null;
-
-				foreach (var valueConverter in converters)
-				{
-					if (valueConverter.CanMap(property.Type) == false)
-						continue;
-
-					converter = valueConverter;
-					break;
-				}
-
-				if (converter == null)
-					continue;
-
-				var converted = converter.Map(property.Type, value);
-
-				property.Assign(target, converted);
-			}
-		}
-
-		private static string GetValueFromSource(IValueSelector[] valueSelectors, ValueSelectorArgs args, PropertyDescriptor property)
-		{
-			foreach (var filter in valueSelectors)
-			{
-				var value = filter.Select(args.With(property));
-
-				if (value != null)
-					return value;
-			}
-
-			return null;
+			builder.Populate(target, configProvider);
 		}
 	}
 }
