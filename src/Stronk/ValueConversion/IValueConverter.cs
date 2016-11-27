a3d69f50beaec23547623efea0d5f6e72b204276
andy <andy@andydote.co.uk>
2016-11-27 21:14:08 +0200

    valueConverter: types into separate files

--- src/Stronk/ValueConversion/IValueConverter.cs
+++ src/Stronk/ValueConversion/IValueConverter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Stronk.ValueConversion
 {
@@ -7,19 +6,5 @@ namespace Stronk.ValueConversion
 	{
 		bool CanMap(Type target);
 		object Map(ValueConverterArgs e);
-	}
-
-	public class ValueConverterArgs
-	{
-		public Type Target { get; }
-		public string Input { get; }
-		public IEnumerable<IValueConverter> OtherConverters { get; }
-
-		public ValueConverterArgs(IEnumerable<IValueConverter> others, Type target, string input)
-		{
-			OtherConverters = others;
-			Target = target;
-			Input = input;
-		}
 	}
 }
---
+++ src/Stronk/ValueConversion/ValueConverterArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stronk.ValueConversion
+{
+	public class ValueConverterArgs
+	{
+		public Type Target { get; }
+		public string Input { get; }
+		public IEnumerable<IValueConverter> OtherConverters { get; }
+
+		public ValueConverterArgs(IEnumerable<IValueConverter> others, Type target, string input)
+		{
+			OtherConverters = others;
+			Target = target;
+			Input = input;
+		}
+	}
+}
