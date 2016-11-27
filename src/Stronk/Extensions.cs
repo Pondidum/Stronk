8ec688a33a26aeb3f86cd7ae76f7873af8f22fd5
andy <andy@andydote.co.uk>
2016-11-27 20:44:31 +0200

    extensons: add fromWebConfig() alias

--- src/Stronk/Extensions.cs
+++ src/Stronk/Extensions.cs
@@ -13,5 +13,17 @@
 
 			builder.Populate(target, configSource);
 		}
+
+		public static void FromWebConfig(this object target)
+		{
+			target.FromWebConfig(new StronkConfiguration());
+		}
+
+		public static void FromWebConfig(this object target, IStronkConfiguration configuration, IConfigurationSource configSource = null)
+		{
+			var builder = new ConfigBuilder(configuration);
+
+			builder.Populate(target, configSource);
+		}
 	}
 }
