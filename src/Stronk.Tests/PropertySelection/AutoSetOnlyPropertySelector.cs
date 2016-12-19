6eca3ac8ab8714ea3942ef1fc2ffcb2f11a53167
andy <andy@andydote.co.uk>
2016-12-19 22:49:15 +0200

    propertySelection: stub out test for setter only auto property selection

---
+++ src/Stronk.Tests/PropertySelection/AutoSetOnlyPropertySelector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using NSubstitute;
+using Shouldly;
+using Stronk.PropertySelection;
+using Xunit;
+
+namespace Stronk.Tests.PropertySelection
+{
+	public class AutoSetOnlyPropertySelectorTests
+	{
+
+		[Fact]
+		public void When_loading_values()
+		{
+			var source = Substitute.For<IConfigurationSource>();
+			source.AppSettings.Returns(new NameValueCollection());
+			source.ConnectionStrings.Returns(new ConnectionStringSettingsCollection());
+			source.AppSettings["Name"] = "Testing";
+			source.AppSettings["Value"] = "16";
+
+			var selector = new AutoSetOnlyPropertySelector();
+
+			var config = new TargetConfig(selector, source);
+
+			config.ShouldSatisfyAllConditions(
+				() => config.Name.ShouldBe("Testing"),
+				() => config.Value.ShouldBe(16)
+			);
+		}
+
+
+		private class TargetConfig
+		{
+			public string Name { get; }
+			public int Value { get; }
+
+			public TargetConfig(IPropertySelector selector, IConfigurationSource source)
+			{
+				this.FromAppConfig(
+					options: new StronkOptions
+					{
+						PropertySelectors = new [] { selector }.ToList()
+					},
+					configSource: source
+				);
+			}
+		}
+	}
+}
---
+++ src/Stronk/PropertySelection/AutoSetOnlyPropertySelector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stronk.PropertySelection
+{
+	public class AutoSetOnlyPropertySelector : IPropertySelector
+	{
+		public IEnumerable<PropertyDescriptor> Select(Type targetType)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
