c6f9610f16c22b5572cf8042b2d2c64115b58e83
andy <andy@andydote.co.uk>
2017-07-02 20:08:15 +0300

    extensions: remove AddBefore and AddAfter - just use linq

--- src/Stronk/Extensions.cs
+++ src/Stronk/Extensions.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Stronk.PropertySelection;
-using Stronk.SourceValueSelection;
-using Stronk.ValueConversion;
 
 namespace Stronk
 {
@@ -42,34 +39,6 @@ namespace Stronk
 			var arguments = string.Join(", ", type.GetGenericArguments().Select(RecurseTypeName));
 
 			return $"{typeName}<{arguments}>";
-		}
-
-		public static void AddBefore<T>(this List<IValueConverter> collection, IValueConverter valueConverter) => InsertBefore(collection, typeof(T), valueConverter);
-		public static void AddBefore<T>(this List<IPropertySelector> collection, IPropertySelector propertySelector) => InsertBefore(collection, typeof(T), propertySelector);
-		public static void AddBefore<T>(this List<ISourceValueSelector> collection, ISourceValueSelector sourceValueSelector) => InsertBefore(collection, typeof(T), sourceValueSelector);
-
-		public static void AddAfter<T>(this List<IValueConverter> collection, IValueConverter valueConverter) => InsertAfter(collection, typeof(T), valueConverter);
-		public static void AddAfter<T>(this List<IPropertySelector> collection, IPropertySelector propertySelector) => InsertAfter(collection, typeof(T), propertySelector);
-		public static void AddAfter<T>(this List<ISourceValueSelector> collection, ISourceValueSelector sourceValueSelector) => InsertAfter(collection, typeof(T), sourceValueSelector);
-
-		private static void InsertBefore<T>(List<T> collection, Type target, T value)
-		{
-			var index = collection.FindIndex(vc => vc.GetType() == target);
-
-			if (index < 0)
-				throw new StronkConfigurationException($"Unable to find a '{target.Name}' to add before");
-
-			collection.Insert(index, value);
-		}
-
-		private static void InsertAfter<T>(List<T> collection, Type target, T value)
-		{
-			var index = collection.FindIndex(vc => vc.GetType() == target);
-
-			if (index < 0)
-				throw new StronkConfigurationException($"Unable to find a '{target.Name}' to add before");
-
-			collection.Insert(index + 1, value);
 		}
 	}
 }
