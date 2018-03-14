e5fee9264ddf93537c01481c12fe6ca55a98fcc6
andy <andydavies2020@gmail.com>
2018-03-14 23:12:34 +0200

    stronkConfig: add tests for configuration sources

--- src/Stronk.Tests/StronkConfigTests.cs
+++ src/Stronk.Tests/StronkConfigTests.cs
@@ -1,4 +1,6 @@
-﻿using Shouldly;
+﻿using System.Collections.Generic;
+using Shouldly;
+using Stronk.ConfigurationSourcing;
 using Stronk.Policies;
 using Xunit;
 
@@ -6,18 +8,42 @@ namespace Stronk.Tests
 {
 	public class StronkConfigTests
 	{
+		private IStronkConfig _config;
+
 		[Fact]
 		public void When_nothing_is_specified()
 		{
-			var config = new StronkConfig() as IStronkConfig;
+			_config = new StronkConfig() as IStronkConfig;
 
-			config.ShouldSatisfyAllConditions(
-				() => config.ConfigSources.ShouldBe(Default.ConfigurationSources),
-				() => config.ValueSelectors.ShouldBe(Default.SourceValueSelectors),
-				() => config.ValueConverters.ShouldBe(Default.ValueConverters),
-				() => config.PropertyWriters.ShouldBe(Default.PropertyWriters),
-				() => config.ErrorPolicy.ShouldBeOfType<ErrorPolicy>()
+			_config.ShouldSatisfyAllConditions(
+				() => _config.ConfigSources.ShouldBe(Default.ConfigurationSources),
+				() => _config.ValueSelectors.ShouldBe(Default.SourceValueSelectors),
+				() => _config.ValueConverters.ShouldBe(Default.ValueConverters),
+				() => _config.PropertyWriters.ShouldBe(Default.PropertyWriters),
+				() => _config.ErrorPolicy.ShouldBeOfType<ErrorPolicy>()
 			);
+		}
+
+		[Fact]
+		public void When_you_specify_a_config_source_it_is_the_only_one_used()
+		{
+			var source = new DictionaryConfigurationSource(new Dictionary<string, string>());
+			_config = new StronkConfig().From.Source(source) as IStronkConfig;
+
+			_config.ConfigSources.ShouldBe(new[] { source });
+		}
+
+		[Fact]
+		public void When_multiple_config_sources_are_selected()
+		{
+			var one = new AppConfigSource();
+			var two = new DictionaryConfigurationSource(new Dictionary<string, string>());
+
+			_config = new StronkConfig()
+				.From.Source(one)
+				.From.Source(two);
+
+			_config.ConfigSources.ShouldBe(new IConfigurationSource[] { one, two });
 		}
 	}
 }
