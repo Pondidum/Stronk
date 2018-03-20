3b2208006afc375d9a635fd3499f957fe6001bef
andy <andydavies2020@gmail.com>
2018-03-20 18:07:46 +0200

    configSources: add environmentVariable source

---
+++ src/Stronk.Tests/ConfigurationSourcing/EnvironmentVariableSourceTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Shouldly;
+using Stronk.ConfigurationSourcing;
+using Xunit;
+
+namespace Stronk.Tests.ConfigurationSourcing
+{
+	public class EnvironmentVariableSourceTests
+	{
+		private readonly EnvironmentVariableSource _source;
+
+		public EnvironmentVariableSourceTests()
+		{
+			_source = new EnvironmentVariableSource();
+		}
+
+		[Fact]
+		public void When_fetching_all_available_variables()
+		{
+			_source.GetAvailableKeys().ShouldNotBeEmpty();
+		}
+
+		[Fact]
+		public void When_getting_an_existing_key()
+		{
+			_source.GetValue("PATH").ShouldNotBeNullOrWhiteSpace();
+		}
+
+		[Fact]
+		public void When_getting_an_existing_value_with_different_casting()
+		{
+			_source.GetValue("PAth").ShouldNotBeNullOrWhiteSpace();
+		}
+
+		[Fact]
+		public void When_getting_a_non_existing_key()
+		{
+			_source.GetValue(Guid.NewGuid().ToString()).ShouldBeNull();
+		}
+	}
+}
---
+++ src/Stronk/ConfigurationSourcing/EnvironmentVariableSource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stronk.ConfigurationSourcing
+{
+	public class EnvironmentVariableSource : IConfigurationSource
+	{
+		private readonly Lazy<Dictionary<string, string>> _variables;
+
+		public EnvironmentVariableSource()
+		{
+			_variables = new Lazy<Dictionary<string, string>>(() => Environment
+				.GetEnvironmentVariables()
+				.Cast<DictionaryEntry>()
+				.ToDictionary(
+					e => (string)e.Key,
+					e => (string)e.Value,
+					StringComparer.OrdinalIgnoreCase));
+		}
+
+		public string GetValue(string key) => _variables.Value.TryGetValue(key, out var value) ? value : null;
+
+		public IEnumerable<string> GetAvailableKeys() => _variables.Value.Keys;
+	}
+}
