4f6718aabc8cef5785bf470165b1bbb2866922d1
andy <andydavies2020@gmail.com>
2018-03-26 21:59:26 +0300

    tests: add scenario for a property being writable by multiple writers

---
+++ src/Stronk.Tests/Scenarios/MultiplePropertyWrites.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Shouldly;
+using Stronk.ConfigurationSources;
+using Stronk.PropertyWriters;
+using Xunit;
+
+namespace Stronk.Tests.Scenarios
+{
+	public class MultiplePropertyWrites
+	{
+		[Fact]
+		public void When_a_property_is_selected_by_two_different_writers()
+		{
+			var config = new StronkConfig()
+				.From.Source(new DictionarySource(new Dictionary<string, string> { { "TestValue", "16" } }))
+				.Write.To(new BackingFieldPropertyWriter())
+				.Write.To(new PrivateSetterPropertyWriter())
+				.Build<MultiWriteConfig>();
+
+			config.AlreadySet.ShouldBe(false);
+			config.TestValue.ShouldBe(16);
+		}
+
+
+		private class MultiWriteConfig
+		{
+			private int _testValue;
+
+			public bool AlreadySet = false;
+
+			public int TestValue
+			{
+				get => _testValue;
+				set
+				{
+					if (_testValue == value)
+						AlreadySet = true;
+
+					_testValue = value;
+					
+				}
+			}
+		}
+	}
+}
