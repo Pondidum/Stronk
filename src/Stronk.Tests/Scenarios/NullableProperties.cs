72eb1d9fba5c8228c3f869de0127db2cef0a2df2
andy <andydavies2020@gmail.com>
2018-04-17 15:42:34 +0300

    tests: add test to cover nullable property assigning

---
+++ src/Stronk.Tests/Scenarios/NullableProperties.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Shouldly;
+using Stronk.ConfigurationSources;
+using Xunit;
+
+namespace Stronk.Tests.Scenarios
+{
+	public class NullableProperties
+	{
+		[Fact]
+		public void When_the_sources_dont_have_a_value()
+		{
+			var values = new DictionarySource(new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase));
+
+			var config = new StronkConfig()
+				.From.Source(values)
+				.Build<NullablePropertyConfig>();
+
+			config.SomeValue.HasValue.ShouldBeFalse();
+		}
+
+		[Fact]
+		public void When_the_source_has_a_value()
+		{
+			var values = new DictionarySource(new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+			{
+				{ "SomeValue", "17" }
+			});
+
+			var config = new StronkConfig()
+				.From.Source(values)
+				.Build<NullablePropertyConfig>();
+
+			config.SomeValue.HasValue.ShouldBeTrue();
+			config.SomeValue.ShouldBe(17);
+		}
+
+		private class NullablePropertyConfig
+		{
+			public Nullable<int> SomeValue { get; private set; }
+		}
+	}
+}
