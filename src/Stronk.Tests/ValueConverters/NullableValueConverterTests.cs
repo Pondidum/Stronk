dfcb6b34e3deab52f265084d5761a32b8a7aec7e
andy <andydavies2020@gmail.com>
2018-04-17 16:11:28 +0300

    valueConverters: add nullableValueConverter

---
+++ src/Stronk.Tests/ValueConverters/NullableValueConverterTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Shouldly;
+using Stronk.ValueConverters;
+using Xunit;
+
+namespace Stronk.Tests.ValueConverters
+{
+	public class NullableValueConverterTests
+	{
+		private readonly NullableValueConverter _converter;
+
+		public NullableValueConverterTests()
+		{
+			_converter = new NullableValueConverter();
+		}
+
+		[Theory]
+		[InlineData(typeof(int?), true)]
+		[InlineData(typeof(int), false)]
+		[InlineData(typeof(bool?), true)]
+		[InlineData(typeof(bool), false)]
+		public void It_can_map_correct_types(Type type, bool expected)
+		{
+			if (expected)
+				_converter.CanMap(type).ShouldBeTrue();
+			else
+				_converter.CanMap(type).ShouldBeFalse();
+		}
+
+		[Fact]
+		public void When_converting_a_value()
+		{
+			_converter
+				.Map(Create<int?>("17"))
+				.ShouldBe(17);
+		}
+
+		[Fact]
+		public void When_converting_null()
+		{
+			_converter
+				.Map(Create<int?>(""))
+				.ShouldBe(null);
+		}
+
+		private static ValueConverterArgs Create<T>(string value, IEnumerable<IValueConverter> others = null) =>
+			new ValueConverterArgs(
+				(message, args) => { },
+				others ?? new[] { new FallbackValueConverter() },
+				typeof(T),
+				value);
+	}
+}
---
+++ src/Stronk/ValueConverters/NullableValueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Stronk.ValueConverters;
+
+namespace Stronk.ValueConverters
+{
+	public class NullableValueConverter : IValueConverter
+	{
+		public bool CanMap(Type target) =>
+			target.IsGenericType &&
+			target.GetGenericTypeDefinition() == typeof(Nullable<>);
+
+		public object Map(ValueConverterArgs e)
+		{
+			if (string.IsNullOrEmpty(e.Input))
+				return null;
+
+			var wrappedType = e.Target.GetGenericArguments().Single();
+			var otherConverters = e.OtherConverters.ToArray();
+
+			var converter = otherConverters.First(c => c.CanMap(wrappedType));
+			var value = converter.Map(new ValueConverterArgs(e.Logger, otherConverters, wrappedType, e.Input));
+
+			return value;
+		}
+	}
+}
