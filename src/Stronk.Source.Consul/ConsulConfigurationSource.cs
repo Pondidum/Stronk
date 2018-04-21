1620561ce01d29c777341707592e8e9386bcf426
andy <andydavies2020@gmail.com>
2018-04-21 21:56:19 +0300

    consul: super basic configsource implementation

---
+++ src/Stronk.Source.Consul/ConsulConfigurationSource.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Consul;
+using Stronk.ConfigurationSources;
+
+namespace Stronk.Source.Consul
+{
+	public class ConsulConfigurationSource : IConfigurationSource
+	{
+		private readonly Func<IConsulClient> _clientFactory;
+
+		public ConsulConfigurationSource(Func<IConsulClient> clientFactory)
+		{
+			_clientFactory = clientFactory;
+		}
+
+		public string GetValue(string key)
+		{
+			using (var client = _clientFactory())
+			{
+				var response = client.KV.Get(key).Result.Response;
+
+				return AsString(response.Value);
+			}
+		}
+
+		public IEnumerable<string> GetAvailableKeys()
+		{
+			using (var client = _clientFactory())
+			{
+				var response = client.KV.List("").Result.Response;
+
+				return response.Select(r => r.Value).Select(AsString).ToArray();
+			}
+		}
+
+		private static string AsString(byte[] bytes) => Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+	}
+}
---
+++ src/Stronk.Source.Consul/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Consul;
+using Stronk.Dsl;
+
+namespace Stronk.Source.Consul
+{
+	public static class Extensions
+	{
+		public static StronkConfig Consul(this SourceExpression self)
+		{
+			return self.Source(new ConsulConfigurationSource(() => new ConsulClient()));
+		}
+	}
+}
