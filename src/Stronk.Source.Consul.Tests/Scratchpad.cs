0bf95495c8bed185d2b4b1169eb3045c07fc22e3
andy <andydavies2020@gmail.com>
2018-04-22 13:39:51 +0300

    tests: add test project for consul

---
+++ src/Stronk.Source.Consul.Tests/Scratchpad.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace Stronk.Source.Consul.Tests
+{
+	public class Scratchpad
+	{
+		private readonly ITestOutputHelper _output;
+
+		public Scratchpad(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+		[Fact]
+		public void When_testing_something()
+		{
+
+		}
+	}
+}
