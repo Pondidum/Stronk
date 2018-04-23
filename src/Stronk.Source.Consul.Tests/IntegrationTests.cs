48c57a9c434982528578965761e6483b3b474e0c
andy <andydavies2020@gmail.com>
2018-04-23 22:34:34 +0300

    consul: support prefixes and queryOptions

--- src/Stronk.Source.Consul.Tests/IntegrationTests.cs
+++ src/Stronk.Source.Consul.Tests/IntegrationTests.cs
@@ -9,7 +9,7 @@ namespace Stronk.Source.Consul.Tests
 {
 	public class IntegrationTests : IDisposable
 	{
-		private readonly ConsulConfigurationSource _source;
+		private ConsulConfigurationSource _source;
 		private readonly ConsulClient _client;
 		private readonly string _prefix;
 
@@ -32,25 +32,60 @@ namespace Stronk.Source.Consul.Tests
 			var key = Guid.NewGuid().ToString();
 			var value = Guid.NewGuid().ToString();
 
-			await _client.KV.Put(new KVPair(_prefix + "/" + key) { Value = Encoding.UTF8.GetBytes(value) });
+			await _client.KV.Put(Pair(key, value));
 
-			_source.GetValue(_prefix + "/" + key).ShouldBe(value);
+			_source.GetValue(Prefixed(key)).ShouldBe(value);
 		}
 
 		[RequiresConsulFact]
 		public async Task When_listing_keys()
 		{
-			await _client.KV.Put(new KVPair(_prefix + "/a") { Value = Encoding.UTF8.GetBytes("one") });
-			await _client.KV.Put(new KVPair(_prefix + "/b") { Value = Encoding.UTF8.GetBytes("two") });
-			await _client.KV.Put(new KVPair(_prefix + "/c") { Value = Encoding.UTF8.GetBytes("three") });
+			await _client.KV.Put(Pair("a", "one"));
+			await _client.KV.Put(Pair("b", "two"));
+			await _client.KV.Put(Pair("c", "three"));
 
 			_source.GetAvailableKeys().ShouldBe(new[]
 			{
-				_prefix + "/a",
-				_prefix + "/b",
-				_prefix + "/c"
+				Prefixed("a"),
+				Prefixed("b"),
+				Prefixed("c")
 			}, ignoreOrder: true);
 		}
+
+		[RequiresConsulFact]
+		public async Task When_using_a_prefix_listing_all_keys_doesnt_include_the_prefix()
+		{
+			_source = new ConsulConfigurationSource(() => new ConsulClient(), prefix: _prefix);
+
+			await _client.KV.Put(Pair("a", "one"));
+			await _client.KV.Put(Pair("b", "two"));
+			await _client.KV.Put(Pair("c", "three"));
+
+			_source.GetAvailableKeys().ShouldBe(new[]
+			{
+				"a",
+				"b",
+				"c"
+			}, ignoreOrder: true);
+		}
+
+		[RequiresConsulFact]
+		public async Task When_using_a_prefix_the_correct_value_is_returned()
+		{
+			_source = new ConsulConfigurationSource(() => new ConsulClient(), prefix: _prefix + "/correct");
+
+			await _client.KV.Put(Pair("correct/a", "correct"));
+			await _client.KV.Put(Pair("incorrect/a", "incorrect"));
+
+			_source.GetValue("a").ShouldBe("correct");
+		}
+
+		private string Prefixed(string key) => _prefix + "/" + key;
+
+		private KVPair Pair(string key, string value) => new KVPair(Prefixed(key))
+		{
+			Value = Encoding.UTF8.GetBytes(value)
+		};
 
 		public void Dispose()
 		{
--- src/Stronk.Source.Consul/ConsulConfigurationSource.cs
+++ src/Stronk.Source.Consul/ConsulConfigurationSource.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Consul;
 using Stronk.ConfigurationSources;
 
@@ -10,34 +11,44 @@ namespace Stronk.Source.Consul
 	public class ConsulConfigurationSource : IConfigurationSource
 	{
 		private readonly Func<IConsulClient> _clientFactory;
+		private readonly QueryOptions _options;
+		private readonly string _prefix;
 
-		public ConsulConfigurationSource(Func<IConsulClient> clientFactory)
+		public ConsulConfigurationSource(Func<IConsulClient> clientFactory, string prefix = null, QueryOptions options = null)
 		{
 			_clientFactory = clientFactory;
+			_options = options ?? QueryOptions.Default;
+			_prefix = WithTrailingSlash(prefix ?? string.Empty);
 		}
 
-		public string GetValue(string key)
+		public string GetValue(string key) => Query(
+			client => client.Get(_prefix + key, _options),
+			result => result != null ? AsString(result.Value) : null);
+
+		public IEnumerable<string> GetAvailableKeys() => Query(
+			client => client.List(_prefix, _options),
+			result => result
+				.Select(TheKey)
+				.Select(WithoutPrefix)
+				.ToArray());
+
+		private TReturn Query<TResponse, TReturn>(Func<IKVEndpoint, Task<QueryResult<TResponse>>> query, Func<TResponse, TReturn> transform)
 		{
 			using (var client = _clientFactory())
-			{
-				var response = client.KV.Get(key).Result.Response;
-
-				return response != null
-					? AsString(response.Value)
-					: null;
-			}
+				return transform(query(client.KV).Result.Response);
 		}
 
-		public IEnumerable<string> GetAvailableKeys()
-		{
-			using (var client = _clientFactory())
-			{
-				var response = client.KV.List("").Result.Response;
+		private static string WithTrailingSlash(string input) =>
+			input.EndsWith("/")
+				? input
+				: input + "/";
 
-				return response.Select(r => r.Key).ToArray();
-			}
-		}
+		private static string AsString(byte[] bytes) =>
+			Encoding.UTF8.GetString(bytes, 0, bytes.Length);
 
-		private static string AsString(byte[] bytes) => Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+		private static string TheKey(KVPair pair) => pair.Key;
+		private string WithoutPrefix(string key) => key.StartsWith(_prefix)
+			? key.Substring(_prefix.Length)
+			: key;
 	}
 }
